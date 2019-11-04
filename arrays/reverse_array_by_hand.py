01f2a6f7efdca3d1a73c47a7150b3ce0bd5ead21
Huxwell <filip.drapejkowski@gmail.com>
2019-11-04 13:03:34 +0100

    Reverse array by hand - to start excercising with arrays manipulations

---
+++ arrays/reverse_array_by_hand.py
@@ -0,0 +1,33 @@
+#!/bin/python3
+# Hackerrank problem : https://www.hackerrank.com/challenges/arrays-ds/problem
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the reverseArray function below.
+
+
+def reverseArray(a):
+    for i in range(int(len(a)/2)):
+        tmp = a[i]
+        a[i] = a[-i-1]
+        a[-i-1] = tmp
+    return a
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    arr_count = int(input())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    res = reverseArray(arr)
+
+    fptr.write(' '.join(map(str, res)))
+    fptr.write('\n')
+
+    fptr.close()
