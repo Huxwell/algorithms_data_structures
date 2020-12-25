5c210c53691248a06ba5a3e4dd8adbe5355000ad
Filip Drapejkowski <filip.drapejkowski@gmail.com>
2020-12-25 23:32:34 +0100

    c++ lower/upper

--- binary_search.py
+++ binary_search.py
@@ -20,3 +20,38 @@ def bin_search_eq(arr, target, mini, maxi): #looking for value equal to target
 
 def bin_search_lower_bound(arr, target, mini, maxi): #looking for first value higher than target
     #pass
+    
+#     UpperBound Implementation in C++:
+
+# int UpperBound(int A[],int N,int K){
+#     int low , high , mid ;
+#     low = 1 ;
+#     high = N ;
+#     while(low <= high){
+#         mid = ( low + high ) / 2 ; // finding middle element 
+#         if(A[mid] > K && ( mid == 1 || A[mid-1] <= K )) // checking conditions for upperbound
+#             return mid ;
+#         else if(A[mid] > K) // answer should be in left part 
+#             high = mid - 1 ;
+#         else                // answer should in right part if it exists
+#             low = mid + 1 ;
+#     }
+#     return mid ; // this will execute when there is no element in the given array which > K
+# }
+# LowerBound Implementation in C++:
+
+# int LowerBound(int A[],int N,int K){
+#     int low , high , mid ;
+#     low = 1 ;
+#     high = N ;
+#     while(low <= high){
+#         mid = ( low + high ) / 2 ; // finding middle element 
+#         if(A[mid] >= K && ( mid == 1 || A[mid-1] < K )) // checking conditions for lowerbound
+#             return mid ;
+#         else if(A[mid] >= K) // answer should be in left part 
+#             high = mid - 1 ;
+#         else                // answer should in right part if it exists
+#             low = mid + 1 ;
+#     }
+#     return mid ; // this will execute when there is no element in the given array which >= K
+# }
